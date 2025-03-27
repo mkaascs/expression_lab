56f3127cc3a94811f1ce8316c8320375ee8f1432
Maxim <teakeyx@gmail.com>
2025-03-27 20:36:13 +0300

    init

---
+++ memory/stats.h
@@ -0,0 +1,19 @@
+#ifndef DB_STATS_H
+#define DB_STATS_H
+#include <sys/_types/_size_t.h>
+
+typedef struct {
+    size_t malloc_state;
+    size_t calloc_state;
+    size_t realloc_state;
+    size_t free_count;
+} DataStats;
+
+void* track_malloc(size_t);
+void* track_calloc(size_t, size_t);
+void* track_realloc(void*, size_t);
+void track_free(void*);
+
+void get_memstat(DataStats*);
+
+#endif //DB_STATS_H
